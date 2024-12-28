9b590c861a8a5243d12cfacd6af7bec8e22f7a7f
MartinQR <quirozrmartin@gmail.com>
2024-12-28 15:58:51 -0600

    top 5

--- src/components/Home/TableTemplates.jsx
+++ src/components/Home/TableTemplates.jsx
@@ -1,6 +1,37 @@
-import { Card } from "@nextui-org/react";
+import { Button, Card } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function TableTemplates() {
+  const APP_URL = import.meta.env.VITE_APP_URL;
+  const [templates, setTemplates] = useState([]);
+  const navigate = useNavigate();
+
+  const getMostRespondedForms = async () => {
+    try {
+      const response = await fetch(`${APP_URL}/form/getMostRespondedForms`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      setTemplates(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getMostRespondedForms();
+  }, []);
+
+
   return (
     <div className="flex flex-col sm:flex-row w-full h-full sm:h-96 my-5 space-y-2 sm:space-x-4 sm:space-y-0">
       <Card className=" w-full sm:w-1/3 h-48 sm:h-full bg-amber-400 rounded-3xl flex items-center justify-center text-center text-3xl md:text-4xl lg:text-6xl leading-none overflow-hidden break-words">
@@ -21,10 +52,23 @@ export default function TableTemplates() {
         <div className="w-2/3 h-full flex items-center justify-center flex-col p-3 space-y-2 sm:space-y-4  ">
           {/* Top 5 popular templates */}
 
-          <div className="text-base sm:text-3xl"> * Template 1</div>
-          <div className="text-base sm:text-3xl"> * Template 2</div>
-          <div className="text-base sm:text-3xl"> * Template 3</div>
-          <div className="text-base sm:text-3xl"> * Template 5</div>
+          {templates?.map((template, i) => (
+            <div key={template?.id} className="flex justify-between items-center w-full">
+              <div>
+                <p className="font-bold">{template?.title}</p>
+                <p className="font-mono">{template?.description}</p>
+              </div>
+              <Button
+                className="bg-amber-400"
+                onClick={() => {
+                  navigate(`/fill-form?idTemplate=${template?.id}`);
+                  setOpen(false);
+                }}
+              >
+                View
+              </Button>
+            </div>
+          ))}
         </div>
       </Card>
     </div>
