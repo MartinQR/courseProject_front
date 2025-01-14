814836a86b4e61b93e4a4fd846777c1aca34c661
MartinQR <quirozrmartin@gmail.com>
2025-01-13 18:28:06 -0600

    Add Salesforce Integration

--- src/components/SalesForce/SalesforceModal.jsx
+++ src/components/SalesForce/SalesforceModal.jsx
@@ -16,39 +16,129 @@ import { createAccountAndContact, getAccessToken } from "../SalesForce/utils";
 
 export default function SalesforceModal({ open, setOpen }) {
   const [token, setToken] = useState("");
+  const [accountData, setAccountData] = useState({ Type: "Customer" });
+  const [contactData, setContactData] = useState({});
+  function updateAccountData(value, input) {
+    setAccountData({
+      ...accountData,
+      [input]: value,
+    });
+  }
 
+  function updateContactData(value, input) {
+    setContactData({ ...contactData, [input]: value });
+  }
+
+  console.log("Account Data", accountData);
+  console.log("Contact Data", contactData);
   return (
     <Modal
       isOpen={open}
       onClose={() => {
         setOpen(false);
       }}
-      className="p-4 ">
-      <ModalContent className="">
+      className="p-4">
+      <ModalContent className="px-2">
         <ModalHeader className="flex justify-center">
           <p className="text-center">Subscribe with Salesforce</p>
         </ModalHeader>
-        <div className="max-h-96 overflow-y-auto my-4 space-y-10">
+        <div className="max-h-96 overflow-y-auto my-4 space-y-10 mx-6">
           <div className="space-y-2">
             <p className="font-semibold text-center">Account Data</p>
-            <Input label="Account Name" size="sm" />
-            <Input label="Industry" size="sm" />
-            <Input label="Phone" size="sm" />
-            <Input label="Website" size="sm" />
+            <Input
+              label="Account Name"
+              size="sm"
+              value={accountData?.name}
+              onChange={(e) => {
+                updateAccountData(e.target.value, "Name");
+              }}
+            />
+            <Input
+              label="Industry"
+              size="sm"
+              value={accountData?.Industry}
+              onChange={(e) => {
+                updateAccountData(e.target.value, "Industry");
+              }}
+            />
+            <Input
+              label="Phone"
+              size="sm"
+              value={accountData?.Phone}
+              onChange={(e) => {
+                updateAccountData(e.target.value, "Phone");
+              }}
+            />
+            <Input
+              label="Website"
+              size="sm"
+              value={accountData?.Website}
+              onChange={(e) => {
+                updateAccountData(e.target.value, "Website");
+              }}
+            />
           </div>
           <div className="space-y-2 mb-6">
             <p className="font-semibold text-center">Contact Data</p>
-            <Input label="First Name" size="sm" />
-            <Input label="Last Name" size="sm" />
-            <Input label="Instagram" size="sm" />
-            <Input label="Fax" size="sm" />
-            <Input label="Email" size="sm" />
-            <Input label="Phone" size="sm" />
+            <Input
+              label="First Name"
+              size="sm"
+              value={contactData?.FirstName}
+              onChange={(e) => {
+                updateContactData(e.target.value, "FirstName");
+              }}
+            />
+            <Input
+              label="Last Name"
+              size="sm"
+              value={contactData?.LastName}
+              onChange={(e) => {
+                updateContactData(e.target.value, "LastName");
+              }}
+            />
+            <Input
+              label="Instagram"
+              size="sm"
+              value={contactData?.Instagram__c}
+              onChange={(e) => {
+                updateContactData(e.target.value, "Instagram__c");
+              }}
+            />
+            <Input
+              label="Fax"
+              size="sm"
+              value={contactData?.Fax}
+              onChange={(e) => {
+                updateContactData(e.target.value, "Fax");
+              }}
+            />
+            <Input
+              label="Email"
+              size="sm"
+              value={contactData?.Email}
+              onChange={(e) => {
+                updateContactData(e.target.value, "Email");
+              }}
+            />
+            <Input
+              label="Phone"
+              size="sm"
+              value={contactData?.Phone}
+              onChange={(e) => {
+                updateContactData(e.target.value, "Phone");
+              }}
+            />
           </div>
         </div>
-        <Button onClick={createAccountAndContact}>
-          Create Account & Contact with SalesForce
-        </Button>
+        <div className="1 w-full flex items-center justify-center">
+          <Button
+            className="w-5/6"
+            onClick={() => {
+              createAccountAndContact(accountData, contactData);
+            }}>
+            Create Account & Contact with SalesForce
+          </Button>
+        </div>
       </ModalContent>
     </Modal>
   );
--- src/components/SalesForce/utils.js
+++ src/components/SalesForce/utils.js
@@ -1,20 +1,24 @@
-const accountData = {
+const defaultAccountData = {
   Name: "Nueva Cuenta#5",
-  Type: "Customer5",
+  Type: "Customer",
   Industry: "Oil",
   Phone: "9876543210",
   Website: "http://www.ejemplo2.com",
 };
-const contactData = {
+const defaultContactData = {
   FirstName: "Martin",
   LastName: "Quiroz",
   Instagram__c: "@quirozrmartin",
-  Fax: "345634563456",  
+  Fax: "345634563456",
   Email: "martin.quiroz@ejemplo.com",
   Phone: "9934017631",
 };
 
-async function createAccountAndContact() {
+async function createAccountAndContact(
+  accountData = defaultAccountData,
+  contactData = defaultContactData
+) {
+  //   const accessToken = await getAccessToken();
   const instanceUrl = "https://quiroz-dev-ed.develop.my.salesforce.com";
   const accessToken =
     "00Daj00000KXNtb!AQEAQITsnynVlmjKi4UcqAw4NF4P4Wmb4mujzxD7afq45YT.sgzuNZ5StslnJTtsQFnGrRa.ixhRG4ygRdKcNWVcDjYwjC6f";
@@ -65,15 +69,56 @@ async function createAccountAndContact() {
   }
 }
 
-
 async function getAccessToken() {
-  console.log("Entra funcion Get Access Token");
-  const clientId =
-    "3MVG9XgkMlifdwVDVJm9VyCYhR.LCe2nNkgVwmgECwshUYFSef08kglhcku50BGmFthXmdSEhI7yAb7fosWaK";
-  const redirectUri = "http://localhost:5173/callback";
-  const authorizationUrl = `https://login.salesforce.com/services/oauth2/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
+  const url = "https://login.salesforce.com/services/oauth2/token";
+  const formData = new URLSearchParams();
 
-  window.location.href = authorizationUrl;
+  formData.append("grant_type", "password");
+  formData.append(
+    "client_id",
+    "3MVG9XgkMlifdwVDVJm9VyCYhR.LCe2nNkgVwmgECwshUYFSef08kglhcku50BGmFthXmdSEhI7yAb7fosWaK"
+  );
+  formData.append(
+    "client_secret",
+    "E60E883BA66AA01A43E986022534DDF7F34FEBFC4FF5EE459CDBF023777CA724"
+  );
+  formData.append("username", "quirozrmartin@gmail.com");
+  formData.append("password", "salesforceQuiroz.2");
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Accept: "application/json",
+        "Cache-Control": "no-cache",
+        Pragma: "no-cache",
+        "User-Agent": "PostmanRuntime/7.42.0",
+        Connection: "keep-alive",
+        "Accept-Encoding": "gzip, deflate, br",
+      },
+      body: formData.toString(),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.access_token;
+  } catch (error) {
+    console.error("Error fetching access token:", error);
+  }
 }
+
+// async function getAccessToken() {
+
+//   const clientId =
+//     "3MVG9XgkMlifdwVDVJm9VyCYhR.LCe2nNkgVwmgECwshUYFSef08kglhcku50BGmFthXmdSEhI7yAb7fosWaK";
+//   const redirectUri = "http://localhost:5173/callback";
+//   const authorizationUrl = `https://login.salesforce.com/services/oauth2/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
+
+//   window.location.href = authorizationUrl;
+// }
 
 export { getAccessToken, createAccountAndContact };
